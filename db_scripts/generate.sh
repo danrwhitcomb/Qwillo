8cfa7e79730895a9d00ea3a601b87c01318181e9
Daniel Whitcomb <danrwhitcomb@gmail.com>
2014-11-20 17:51:48 -0500

    Updated DB scripts and credentials for MongoHQ, config file not in repo

--- db_scripts/generate.sh
+++ db_scripts/generate.sh
@@ -1,10 +1,54 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+	echo "Error:"
+	echo "generate.sh [build type]"
+	exit 0
+fi
+
+
+echo "Requirements: "
+echo "MongoDb - database utility"
+echo "JQ - JSON parse utility"
+echo
+echo "This script will make irreversible changes to the"
+echo "database at the connection string within system/config.json"
+echo "Type either 1 or 2"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit 1;;
+    esac
+done
+
+configFile="../system/config.json"
+
+{
+	mongoConnection=`jq .$1.db.connectionString < ../system/config.json`
+	host=`jq .$1.db.host < $configFile | tr -d '"'`
+	port=`jq .$1.db.port < $configFile | tr -d '"'`
+	username=`jq .$1.db.username < $configFile | tr -d '"'`
+	password=`jq .$1.db.password < $configFile | tr -d '"'`
+	database=`jq .$1.db.database < $configFile | tr -d '"'`
+	echo "Host: $host"
+	echo "Port: $port"
+	echo "Username: $username"
+	echo "Password: $password"
+	echo "database: $database"
+
+} || {
+	echo "Could not find config.json or it is formatted improperly"
+	exit 0
+}
+
 echo "Wiping former database..."
-mongo local_mongoose --eval "db.dropDatabase();"
+echo $host:$port/$database
+mongo $host:$port/$database -u $username -p $password --eval "db.topics.drop();"
+mongo $host:$port/$database -u $username -p $password --eval "db.categories.drop();"
+mongo $host:$port/$database -u $username -p $password --eval "db.posts.drop();"
 
 echo "Writing Categories to database"
-mongoimport --db local_mongoose --collection categories --file category.json
+mongoimport --host $host --port $port --db $database -username $username -password $password --collection categories --file "category.json"
 
 echo "Fetching wikipedia most visisted..."
 wget -O pages.html http://en.wikipedia.org/wiki/User:West.andrew.g/Popular_pages
@@ -13,6 +57,7 @@ echo "Parsing Links..."
 python htmlparser.py pages.html topics.json
 
 echo "Writing Topics to database"
-mongoimport --db local_mongoose --collection topics --file topics.json
+mongoimport --host $host --port $port --db $database -username $username -password $password --collection topics --file "topics.json"
 echo "Complete"
 
+exit 0
